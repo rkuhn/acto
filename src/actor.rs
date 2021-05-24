67823afd43c9508eb1b9ff05d0315678a4b944c7
Roland Kuhn <rk@rkuhn.info>
2021-05-24 10:32:08 +0200

    remove old stuff

--- src/actor.rs
+++ src/actor.rs
@@ -1,25 +1,74 @@
 use ::tokio::sync::oneshot;
 use anyhow::Result;
-use std::{future::Future, pin::Pin, sync::Arc};
+use derive_more::{Display, Error};
+use parking_lot::Mutex;
+use std::{
+    future::Future,
+    pin::Pin,
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc,
+    },
+    task::{Poll, Waker},
+};
 
-pub struct ActorRef<M>(Arc<dyn Fn(M) + Send + Sync + 'static>);
+/// An ActorRef is the sending side of the actor’s mailbox, it can be freely cloned.
+///
+/// When the last `ActorRef` for an actor has been dropped, only the actor itself can
+/// fabricate a new one. And if the actor is waiting for new messages from its mailbox
+/// before a new `ActorRef` is created, the actor would be stuck in this state forever.
+/// Therefore, the actor’s `receive()` function returns an error in this case, which
+/// will usually lead to the actor shutting down.
+pub struct ActorRef<M>(Arc<ActorRefInner<M>>);
 
 impl<M> Clone for ActorRef<M> {
     fn clone(&self) -> Self {
+        self.0.count.fetch_add(1, Ordering::SeqCst);
         Self(self.0.clone())
     }
 }
 
-impl<M> ActorRef<M> {
-    pub fn new(f: Box<dyn Fn(M) + Send + Sync + 'static>) -> Self {
-        Self(f.into())
-    }
-
-    pub fn tell(&self, msg: M) {
-        (self.0)(msg);
+impl<M> Drop for ActorRef<M> {
+    fn drop(&mut self) {
+        let prev = self.0.count.fetch_sub(1, Ordering::SeqCst);
+        if prev == 1 {
+            // we’re the last external reference, so wake up the Actor if it is waiting for us
+            if let Some(waker) = self.0.waker.lock().take() {
+                waker.wake();
+            }
+        }
     }
 }
 
+impl<M> ActorRef<M> {
+    pub fn new(tell: Box<dyn Fn(M) + Send + Sync + 'static>) -> Self {
+        Self(Arc::new(ActorRefInner {
+            count: AtomicUsize::new(0),
+            waker: Mutex::new(None),
+            tell,
+        }))
+    }
+
+    pub fn tell(&self, msg: M) {
+        (self.0.tell)(msg);
+    }
+}
+
+struct ActorRefInner<M> {
+    count: AtomicUsize,
+    waker: Mutex<Option<Waker>>,
+    tell: Box<dyn Fn(M) + Send + Sync + 'static>, // TODO get rid of the box (requires unsafe)
+}
+
+/// The context in which an actor is running
+///
+/// This context allows the actor to
+///
+/// - receive messages
+/// - get its own address (i.e. [`ActorRef`](struct.ActorRef.html))
+/// - spawn new actors
+///
+/// The definition of child actors is best donw using the [`actor`](macro.actor.html) macro.
 pub struct Context<M> {
     recv: Box<dyn Receiver<M>>,
     aref: ActorRef<M>,
@@ -45,14 +94,27 @@ impl<M: Send + 'static> Context<M> {
         }
     }
 
-    pub async fn receive(&mut self) -> Result<M> {
-        self.recv.receive().await
+    /// Receive the next message from the mailbox, possibly waiting for it
+    ///
+    /// This method’s return value should always be immediately `.await`ed, it has no other use.
+    pub fn receive(&mut self) -> ReceiveFuture<'_, M> {
+        ReceiveFuture {
+            aref: &self.aref.0,
+            fut: self.recv.receive(),
+        }
     }
 
+    /// The actors own address a.k.a. ActorRef
+    ///
+    /// The actor can put this `ActorRef` into messages to send it to other actors so that they
+    /// can contact this actor back later.
     pub fn me(&self) -> ActorRef<M> {
         self.aref.clone()
     }
 
+    /// Spawn a future for the purpose of running a child actor
+    ///
+    /// This method is best used via the [`actor`](macro.actor.html) macro.
     pub fn spawn(
         &self,
         fut: Pin<Box<dyn Future<Output = Result<()>> + Send + 'static>>,
@@ -61,14 +123,52 @@ impl<M: Send + 'static> Context<M> {
     }
 }
 
+pub struct ReceiveFuture<'a, M: Send + 'static> {
+    aref: &'a ActorRefInner<M>,
+    fut: &'a mut (dyn Future<Output = Result<M>> + Send + Unpin + 'a),
+}
+
+impl<'a, M: Send + 'static> Future for ReceiveFuture<'a, M> {
+    type Output = Result<M>;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<Self::Output> {
+        match Pin::new(&mut *self.fut).poll(cx) {
+            Poll::Ready(x) => Poll::Ready(x),
+            Poll::Pending => {
+                if self.aref.count.load(Ordering::SeqCst) == 0 {
+                    // observing no external address means that there cannot be one created, either:
+                    // we hold an exclusive reference on the Context (via self.fut)
+                    Poll::Ready(Err(NoActorRef.into()))
+                } else {
+                    *self.aref.waker.lock() = Some(cx.waker().clone());
+                    // in case the last ActorRef was dropped between the check and installing the waker,
+                    // we must now re-check
+                    if self.aref.count.load(Ordering::SeqCst) == 0 {
+                        Poll::Ready(Err(NoActorRef.into()))
+                    } else {
+                        Poll::Pending
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug, Display, Error)]
+#[display(fmt = "cannot receive: no external ActorRef for this actor")]
+pub struct NoActorRef;
+
 pub trait Receiver<M: Send + 'static>: Send {
+    // this trait is only necessary because FnMut doesn’t make its argument’s self reference lifetime available
     fn receive(&mut self) -> &mut (dyn Future<Output = Result<M>> + Send + Unpin + '_);
 }
 
+/// Factory for mailboxes, which usually are MPSC queues under the hood
 pub trait Mailbox {
     fn make_mailbox<M: Send + 'static>(&self) -> (ActorRef<M>, Box<dyn Receiver<M>>);
 }
 
+/// Facility for spawning a particular kind of Future that is used to run actors
 pub trait Spawner: Send + Sync + 'static {
     fn spawn(
         &self,
@@ -76,7 +176,7 @@ pub trait Spawner: Send + Sync + 'static {
     ) -> oneshot::Receiver<()>;
 }
 
-mod tokio {
+pub mod tokio {
     use ::tokio::sync::oneshot;
     use futures::Future;
     use std::{pin::Pin, task::Poll};
@@ -137,34 +237,94 @@ mod tokio {
     }
 }
 
+/// Define and spawn a new Actor
+///
+/// The first form allows spawning a top-level actor defined in an `async fn`:
+///
+/// ```
+/// use acto::{actor, Context, Result, TokioMailbox, TokioSpawner};
+///
+/// async fn my_actor(mut ctx: Context<String>, me: String) -> Result<()> {
+///     loop {
+///         let msg = ctx.receive().await?;
+///         println!("{} got msg: {}", me, msg);
+///     }
+/// }
+///
+/// #[tokio::main]
+/// async fn main() {
+///     let (aref, join_handle) = actor!(TokioMailbox, TokioSpawner, fn my_actor(ctx, "Fred".to_owned()));
+/// }
+/// ```
+///
+/// The second form allows spawning a top-level actor defined inline:
+///
+/// ```
+/// use acto::{actor, Context, TokioMailbox, TokioSpawner};
+///
+/// #[tokio::main]
+/// async fn main() {
+///     let me = "Barney".to_owned();
+///     let (aref, join_handle) = actor!(TokioMailbox, TokioSpawner, |ctx| {
+///         loop {
+///             let msg: String = ctx.receive().await?;
+///             println!("{} got msg: {}", me, msg);
+///         }
+///     });
+/// }
+/// ```
+///
+/// The third form is for spawning actors within other actors. The `ctx` variable can be named differently,
+/// but it must be the name of the enclosing actor’s `Context`. It will intentionally shadow this name within
+/// the child actor, to make it obvious that the parent’s `Context` cannot be used there.
+///
+/// ```
+/// use acto::{actor, Context, Result, TokioMailbox, TokioSpawner};
+///
+/// async fn my_actor(mut ctx: Context<String>, me: String) -> Result<()> {
+///     loop {
+///         let msg = ctx.receive().await?;
+///         let (aref, _join_handle) = actor!(TokioMailbox, |ctx| {
+///             let msg = ctx.receive().await?;
+///             println!("servant: {}", msg);
+///             // just to demonstrate that when the external ActorRef is dropped, this stops
+///             let _ = ctx.receive().await?;
+///             println!("servant done");
+///         });
+///         aref.tell(format!("{} got msg: {}", me, msg));
+///     }
+/// }
+/// ```
 #[macro_export]
 macro_rules! actor {
     ($mailbox:path, $spawner:path, fn $f:ident($ctx:ident$(,$arg:expr)*)) => {{
-        let spawner = ::std::sync::Arc::new($spawner);
-        let $ctx = $crate::actor2::Context::new($mailbox, spawner.clone());
-        let aref = $ctx.me();
+        use $crate::Spawner;
+        let _spawner = ::std::sync::Arc::new($spawner);
+        let $ctx = $crate::Context::new($mailbox, _spawner.clone());
+        let _aref = $ctx.me();
         let fut = Box::pin($f($ctx, $($arg),*));
-        (aref, spawner.spawn(fut))
+        (_aref, _spawner.spawn(fut))
     }};
     ($mailbox:path, $spawner:path, |$ctx:ident| $code:block) => {{
-        let spawner = ::std::sync::Arc::new($spawner);
-        let mut $ctx = $crate::actor2::Context::new($mailbox, spawner.clone());
-        let aref = $ctx.me();
+        use $crate::Spawner;
+        let _spawner = ::std::sync::Arc::new($spawner);
+        let mut $ctx = $crate::Context::new($mailbox, _spawner.clone());
+        let _aref = $ctx.me();
         let fut = Box::pin(async move {
             $code
             Ok(())
         });
-        (aref, spawner.spawn(fut))
+        (_aref, _spawner.spawn(fut))
     }};
     ($mailbox:path, |$ctx:ident| $code:block) => {{
         let (fut, aref) = {
             let mut $ctx = $ctx.inherit($mailbox);
-            let aref = $ctx.me();
+            let _aref = $ctx.me();
             let fut = Box::pin(async move {
                 $code
                 Ok(())
             });
-            (fut, aref)
+            (fut, _aref)
         };
         (aref, $ctx.spawn(fut))
     }}
@@ -174,9 +334,11 @@ macro_rules! actor {
 mod tests {
     use super::{
         tokio::{Mailbox, Spawner},
-        ActorRef, Context, Spawner as _,
+        ActorRef, Context, NoActorRef,
     };
     use anyhow::Result;
+    use futures::poll;
+    use std::{task::Poll, thread::sleep, time::Duration};
     use tokio::sync::oneshot;
 
     async fn actor(mut ctx: Context<(String, ActorRef<String>)>) -> Result<()> {
@@ -193,7 +355,7 @@ mod tests {
 
     #[tokio::test]
     async fn smoke() {
-        let (aref, _h) = actor!(Mailbox, Spawner, fn actor(ctx));
+        let (aref, join_handle) = actor!(Mailbox, Spawner, fn actor(ctx));
 
         let (tx, rx) = oneshot::channel();
         let (receiver, jr) = actor!(Mailbox, Spawner, |ctx| {
@@ -212,5 +374,32 @@ mod tests {
         aref.tell(("Barney".to_owned(), receiver));
         assert_eq!(rx.await.unwrap(), "Hello Barney!");
         jr.await.unwrap();
+
+        drop(aref);
+        join_handle.await.unwrap();
+    }
+
+    #[tokio::test]
+    async fn dropped() {
+        let (tx, mut rx) = oneshot::channel();
+        let (aref, handle) = actor!(Mailbox, Spawner, |ctx| {
+            let result: Result<()> = ctx.receive().await;
+            let _ = tx.send(result);
+        });
+
+        sleep(Duration::from_millis(200));
+        match poll!(&mut rx) {
+            Poll::Pending => {}
+            x => panic!("unexpected result: {:?}", x),
+        }
+
+        drop(aref);
+        handle.await.unwrap();
+        let err = match poll!(rx) {
+            Poll::Ready(Ok(e)) => e.unwrap_err(),
+            x => panic!("unexpected poll result: {:?}", x),
+        };
+        err.downcast::<NoActorRef>()
+            .unwrap_or_else(|e| panic!("unexpected error type: {}", e));
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,9 @@
-//! Light-weight actor library for Rust
+//! Runtime-agnostic actor library for Rust
 //!
 //! Currently in early alpha stage: supports tokio for execution, currently only uses the MPSC channel (even for the SPSC API), should probably use feature flags, and still is quite slow.
-//!
-//! Please check out [the example](https://github.com/Actyx/actor/blob/master/examples/pingpong.rs)
 
-use std::{future::Future, marker::PhantomData, sync::Arc};
+mod actor;
 
-mod actor_id;
-mod mailbox;
-mod mpsc;
-mod pod;
-mod sender;
-pub mod spawn;
-mod spsc;
-mod supervisor;
-
-pub mod actor;
-pub mod actor2;
-
-pub use actor_id::ActorId;
-pub use mailbox::{Mailbox, SenderGone};
-pub use mpsc::MPSC;
-pub use pod::{MpQueue, Queue};
-pub use sender::{DynSender, Sender};
-pub use spsc::SPSC;
-pub use supervisor::Supervisor;
-
-pub struct Actor<RT, A, Fut, Msg, S>(
-    PhantomData<RT>,
-    PhantomData<A>,
-    PhantomData<Fut>,
-    PhantomData<Msg>,
-    PhantomData<S>,
-);
-
-impl<RT, A, Fut, Msg, S> Actor<RT, A, Fut, Msg, S>
-where
-    RT: spawn::Spawn,
-    Msg: Send,
-    Fut: Future + Send + 'static,
-    S: Supervisor<Fut::Output>,
-{
-    pub fn spawn<Q>(rt: &RT, actor: A, supervisor: S) -> Sender<Q>
-    where
-        A: FnOnce(Mailbox<Q>) -> Fut,
-        Q: Queue<Msg = Msg>,
-    {
-        let pod = Arc::new(pod::Pod::new());
-        let fut = actor(Mailbox::new(pod.clone()));
-        let fut = async {
-            let result = fut.await;
-            supervisor.notify(result);
-        };
-        rt.spawn(fut);
-        Sender::new(pod)
-    }
-}
+pub use actor::tokio::{Mailbox as TokioMailbox, Spawner as TokioSpawner};
+pub use actor::{ActorRef, Context, Mailbox, Spawner};
+pub use anyhow::Result;
